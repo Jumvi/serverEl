e73fa91ecc3e957819404344a9d9ca30ee6b8e18
Jumvi <judahmvi@gmail.com>
2024-06-07 21:10:33 +0100

    update pull

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
 
-
-<<<<<<< HEAD
 const {getLogin,verifyOtp,forgotpassword,resetPassword} = require('../controllers/auth');
 
 
