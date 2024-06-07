d38bdd4cad6a10207b283b1527eae677edecaebb
Jumvi <judahmvi@gmail.com>
2024-06-07 20:11:33 +0100

    update pull

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -23,7 +23,6 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-
 });
 
 const emailSchema = Joi.string()
